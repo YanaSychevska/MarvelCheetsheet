bee93cdb63a85bf88bdf03040a309c2125e8a10d
YanaSychevska <yanasychevska@yahoo.com>
2023-09-08 18:10:52 -0400

    Initial Commit

---
+++ MarvelCheetsheet/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  MarvelCheetsheet
+//
+//  Created by YanaSychevska on 08.09.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MarvelCheetsheet/MarvelCheetsheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvelCheetsheetApp.swift
+//  MarvelCheetsheet
+//
+//  Created by YanaSychevska on 08.09.23.
+//
+
+import SwiftUI
+
+@main
+struct MarvelCheetsheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
